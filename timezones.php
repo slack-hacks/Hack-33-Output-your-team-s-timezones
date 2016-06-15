d6ee3b2e09c018f3e0a0bf07089c7d3b1bc6bf41
Mike Street <mikestreety@gmail.com>
2016-06-15 23:59:32 +0100

    Typo and formatting updates

--- timezones.php
+++ timezones.php
@@ -3,10 +3,10 @@
 	// Your oAuth Token
 	$token = '[AUTH TOKEN]';
 
-	// Set the timezone independant of the server
+	// Set the timezone independent of the server
 	date_default_timezone_set("UTC");
 
-	// Get the channel ID
+	// Get the channel ID the script was triggered from
 	$channelID = $_POST['channel_id'];
 
 	// Include slack library from https://github.com/10w042/slack-api
@@ -15,7 +15,7 @@
 	// Create new Slack instance
 	$Slack = new Slack($token);
 
-	// Ge the cuurent channel and user
+	// Get the cuurent channel and user
 	$channel = $Slack->call('channels.info', array('channel' => $channelID));
 
 	// Get the time right now
@@ -28,37 +28,39 @@
 	foreach($channel['channel']['members'] as $mid) {
 
 		// Create member instance with ID
-		$member = $Slack->call('users.info', array('user' => $mid));
+		$user = $Slack->call('users.info', array('user' => $mid));
 
-		// if the user is a bot, or doesn't have a timezone offset, skip them
-		if(($member['user']['is_bot'] == true) || (!isset($member['user']['tz_offset']))) {
+		// If the user is a bot, or doesn't have a timezone offset, skip them
+		if(($user['user']['is_bot'] == true) || (!isset($user['user']['tz_offset']))) {
 			continue;
 		}
 
-		// Work out offset in hours
-		$userOffset = ($member['user']['tz_offset'] / 60) / 60;
+		// Work out offset in hours from seconds
+		$userOffset = ($user['user']['tz_offset'] / 60) / 60;
+
+		// Get the name of the user
+		$name = ($user['user']['real_name']) ? $user['user']['real_name'] : $user['user']['name'];
+
+		// Add the timezeone offset to current time
+		$userTime = $now + $user['user']['tz_offset'];
 
 		// Create an array key based on offset (so we can sort) and add 12 (as it could be -11 at worst)
 		$key = $userOffset + 12;
 
-		// Get the name of the user
-		$name = ($member['user']['real_name']) ? $member['user']['real_name'] : $member['user']['name'];
-
-		// Add the timezeone offset to current time
-		$memberTime = $now + $member['user']['tz_offset'];
-
 		// Append the details to the array as the key
 		$times[$key][] = '*' . $name . '*: ' .
-		date('h:i A', $memberTime) . ' local time on ' . date('jS M', $memberTime) .
-		' (UTC' . sprintf("%+d", $userOffset) . ' hours _' . $member['user']['tz_label'] . '_)';
+		date('h:i A', $userTime) . ' local time on ' . date('jS M', $userTime) .
+		' (UTC' . sprintf("%+d", $userOffset) . ' hours _' . $user['user']['tz_label'] . '_)';
 
 	}
 
 	// Sort array items by key
 	ksort($times);
 
-	// Flatten the array and implode it - seperated by new lines
-	$text = implode("\n", call_user_func_array('array_merge', $times));
+	// Flatten the array and implode it - separated by new lines
+	$text = implode("\n", call_user_func_array(
+		'array_merge', $times
+	));
 
 	// Post back to the channel
 	$Slack->call('chat.postMessage', array(
